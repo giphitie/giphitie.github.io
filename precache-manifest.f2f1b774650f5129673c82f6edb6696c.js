6362927fda23f7f26d66728490d98ab783a847b6
Gifty Osei <gifty18osei@gmail.com>
2021-08-18 11:33:30 -0600

    Updates

---
+++ precache-manifest.f2f1b774650f5129673c82f6edb6696c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860942a36d5139279231638586510e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a126229402a379ee61d4",
+    "url": "/static/js/2.fcfe8bcb.chunk.js"
+  },
+  {
+    "revision": "30bbab77a86745cd07c4dfeb36f1b58a",
+    "url": "/static/js/2.fcfe8bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa707b68df489de38634",
+    "url": "/static/js/main.260b6f3b.chunk.js"
+  },
+  {
+    "revision": "0a304fc84fd949804df2",
+    "url": "/static/js/runtime-main.bc5dd276.js"
+  },
+  {
+    "revision": "bfe1cbf305d7b215b18a9922808dadd9",
+    "url": "/static/media/bg_1.bfe1cbf3.jpg"
+  },
+  {
+    "revision": "792e035e340fe634baa39eb46a6e7a4a",
+    "url": "/static/media/me_1.792e035e.png"
+  }
+]);
